b5eb1e47a537e95aa9429292f1e02876815edd20
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2021-04-06 16:02:16 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'profile/:userId', component: UserProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
