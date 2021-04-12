7b4c56e79d6786066d196ab7381d9f05e4c1acd6
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-04-13 00:30:31 +0300

    Article styles

--- src/app/modules/user-side/home/home.component.ts
+++ src/app/modules/user-side/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../../../services/article.service';
-import {Article} from '../../../shared/interfaces/article';
+import { Article } from '../../../shared/interfaces/article';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
