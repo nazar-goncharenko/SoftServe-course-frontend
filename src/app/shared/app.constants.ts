40b8267a3e7d3f77555521eafa9c7ca7747839e0
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-04-16 00:58:55 +0300

    port change to 8082

--- src/app/shared/app.constants.ts
+++ src/app/shared/app.constants.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 
 export class AppConstants {
-   public static URL_PORT = '8081';
+   public static URL_PORT = '8082';
    public static API_URL = `http://localhost:${AppConstants.URL_PORT}`;
    public static MEDIA_PATH = '';
 }
