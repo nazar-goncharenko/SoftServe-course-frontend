36c189f89e4c039278ef9b2ab259691dcbc60c99
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-04-26 10:32:39 +0300

    Conflicts resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './modules/user-side/home/home.component';
-import {ModulesModule} from './modules/modules.module';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
 import {ModuleModule} from './modules/module.module';
@@ -14,13 +13,12 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ModulesModule,
+        ModuleModule,
         FontAwesomeModule,
         HttpClientModule,
         NoopAnimationsModule,
