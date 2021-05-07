49e09caf958bb56410e3c35507303cbb4403d8e7
techsky <55064294+nazar-goncharenko@users.noreply.github.com>
2021-05-07 18:04:13 +0300

    fix interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const basicHeader = JSON.parse(JSON.stringify(localStorage.getItem('Authorization')));
+        const basicHeader = JSON.parse(JSON.stringify(sessionStorage.getItem('Authorization')));
 
         if (basicHeader) {
             req = req.clone({
